e1d4e31cb0ed583ce306bad5c7a86d9a75d09f21
Darren <darren09@live.ca>
2023-04-12 16:58:04 -0400

    Added sticky Nav bar.

--- Frontend/Public/src/resetpassword.php
+++ Frontend/Public/src/resetpassword.php
@@ -11,50 +11,59 @@
     <script src="../assets/js/resetpassword.js"></script>
 </head>
 <body>
-<!-- Navbar -->
-<nav class="navbar">
-    <!-- Logo -->
-    <img src="../assets/img/DCA_logo.png" alt="Logo">
-    <!-- Login button -->
-    <div class="login-button">
-        <i class="fas fa-lock-open"></i>
-        <i class="fas fa-question-circle"></i>
+
+    <!-- Navbar -->
+    <nav class="navbar sticky">
+        <!-- Logo -->
+        <div class="logo-container">
+            <img src="../assets/img/logo.png" alt="Logo">
+        </div>
+        <!-- Login button -->
+        <div class="login-button">
+            <span id="UserEmail"></span>
+            <span class="tooltip" data-tooltip="Logout">
+                    <button id="login-button"><i class="fas fa-lock-open"></i></button>
+                    <div class="tooltip-text">Login</div>
+                </span>
+            <span class="tooltip" data-tooltip="Support">
+                    <button id="Support-button"><i class="fas fa-question-circle"></i></button>
+                    <div class="tooltip-text">Support</div>
+                </span>
+        </div>
+    </nav>
+
+    <!-- Login form -->
+    <div class="login-container">
+        <div class="login-form">
+            <form>
+                <div class="form-group">
+                    <label id="form-label">Forgot your password...</label>
+                </div>
+                <div class="form-group">
+                    <input id="email-input" type="email" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <input id="current-password-input" type="password" placeholder="Current Password">
+                </div>
+                <div class="form-group">
+                    <input id="new-password-input" type="password" placeholder="New Password">
+                </div>
+                <div class="form-group">
+                    <input id="confirm-new-password-input" type="password" placeholder="Confirm New Password">
+                </div>
+                <div class="form-group">
+                    <button id="resetpassword-button" type="submit">Reset Password</button>
+                </div>
+                <div class="form-group">
+                    <button type="button" class="signup-button" onclick="window.location.href='/index2.php'">Back to Login</button>
+                </div>
+            </form>
+        </div>
     </div>
-</nav>
 
-<!-- Login form -->
-<div class="login-container">
-    <div class="login-form">
-        <form>
-            <div class="form-group">
-                <label id="form-label">Forgot your password...</label>
-            </div>
-            <div class="form-group">
-                <input id="email-input" type="email" placeholder="Email">
-            </div>
-            <div class="form-group">
-                <input id="current-password-input" type="password" placeholder="Current Password">
-            </div>
-            <div class="form-group">
-                <input id="new-password-input" type="password" placeholder="New Password">
-            </div>
-            <div class="form-group">
-                <input id="confirm-new-password-input" type="password" placeholder="Confirm New Password">
-            </div>
-            <div class="form-group">
-                <button id="resetpassword-button" type="submit">Reset Password</button>
-            </div>
-            <div class="form-group">
-                <button type="button" class="signup-button" onclick="window.location.href='/index2.php'">Back to Login</button>
-            </div>
-        </form>
-    </div>
-</div>
-
-
-<!-- Footer -->
-<footer class="footer">
-    <span>Copyright &copy; 2023 DCA</span>
-</footer>
+    <!-- Footer -->
+    <footer class="footer">
+        <span>Copyright &copy; 2023 DCA</span>
+    </footer>
 </body>
 </html>
