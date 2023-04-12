522af8f1a07d276cb19881911f92cda95a9ca77f
Darren <darren09@live.ca>
2023-04-12 16:56:52 -0400

    Added contact page. Form submits requests to database.

--- Frontend/Public/src/support.php
+++ Frontend/Public/src/support.php
@@ -9,12 +9,19 @@
 
     <link rel="stylesheet" type="text/css" href="../assets/css/navbar.css">
     <link rel="stylesheet" type="text/css" href="../assets/css/hamburgermenu.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../assets/css/contact.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico">
+
+    <!-- Highcharts   -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/data.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../assets/js/sessionkeycheck.js"></script>
-    <script src="../assets/js/data.js"></script>
+    <script src="../assets/js/contact.js"></script>
 
     <script>
         document.addEventListener("DOMContentLoaded", function() {
@@ -39,54 +46,93 @@
     </script>
 </head>
 <body>
-<!-- Navbar -->
-<nav class="navbar">
-    <!-- Hamburger menu icon -->
-    <button id="menu-toggle" class="menu-toggle">
-        <i class="fas fa-bars"></i>
-    </button>
-    <!-- Logo -->
-    <div class="logo-container">
-        <img src="../assets/img/DCA_logo.png" alt="Logo">
-    </div>
-    <!-- Login button -->
-    <div class="login-button">
+    <!-- Navbar -->
+    <nav class="navbar sticky">
+        <!-- Hamburger menu icon -->
+        <button id="menu-toggle" class="menu-toggle">
+            <i class="fas fa-bars"></i>
+        </button>
+        <!-- Logo -->
+        <div class="logo-container">
+            <img src="../assets/img/logo.png" alt="Logo">
+        </div>
+        <!-- Login button -->
+        <div class="login-button">
+            <span id="UserEmail"></span>
             <span class="tooltip" data-tooltip="Logout">
-                <button id="login-button">
-                <i class="fas fa-lock"></i>
-            </button>
+                <button id="login-button"><i class="fas fa-lock"></i></button>
                 <div class="tooltip-text">Logout</div>
             </span>
-        <i class="fas fa-question-circle"></i>
+            <span class="tooltip" data-tooltip="Support">
+                <button id="Support-button"><i class="fas fa-question-circle"></i></button>
+                <div class="tooltip-text">Support</div>
+            </span>
+        </div>
+    </nav>
+
+    <!-- Menu -->
+    <div id="menu-container" class="menu-container">
+        <ul class="menu">
+            <a href="homepage.php#Demand"><li><i class="fas fa-faucet-drip  menuicons"></i>Demand</li></a>
+            <a href="consumption.php#Consumption"><li><i class="fas fa-glass-water menuicons"></i>Consumption</li></a>
+            <a href="#"><li><i class="fas fa-bug menuicons"></i>N/A</li></a>
+            <a href="#"><li><i class="fas fa-bug menuicons"></i>N/A</li></a>
+            <a href="#"><li><i class="fas fa-bug menuicons"></i>N/A</li></a>
+            <a href="#"><li><i class="fas fa-bug menuicons"></i>N/A</li></a>
+            <a href="#"><li><i class="fas fa-bug menuicons"></i>N/A</li></a>
+            <a href="#"><li><i class="fas fa-bug menuicons"></i>N/A</li></a>
+            <a href="configuration.php#Configuration"><li><i class="fas fa-cog menuicons"></i>Configuration</li></a>
+            <a href="about.php#About"><li><i class="fas fa-graduation-cap menuicons"></i>About</li></a>
+            <a href="support.php#Support"><li><i class="fas fa-question-circle menuicons"></i>Support</li></a>
+        </ul>
     </div>
-</nav>
 
-<!-- Menu -->
-<div id="menu-container" class="menu-container">
-    <ul class="menu">
-        <li><i class="fas fa-lock menuicons"></i><a href="homepage.php#Demand">Demand</a></li>
-        <li><i class="fas fa-cog menuicons"></i><a href="consumption.php#Consumption">Consumption</a></li>
-        <li><i class="fas fa-bug menuicons"></i><a href="#">N/A</a></li>
-        <li><i class="fas fa-bug menuicons"></i><a href="#">N/A</a></li>
-        <li><i class="fas fa-bug menuicons"></i><a href="#">N/A</a></li>
-        <li><i class="fas fa-bug menuicons"></i><a href="#">N/A</a></li>
-        <li><i class="fas fa-bug menuicons"></i><a href="#">N/A</a></li>
-        <li><i class="fas fa-bug menuicons"></i><a href="#">N/A</a></li>
-        <li><i class="fas fa-bug menuicons"></i><a href="#">N/A</a></li>
-        <li><i class="fas fa-lock menuicons"></i><a href="configuration.php#Configuration">Configuration</a></li>
-        <li><i class="fas fa-graduation-cap menuicons"></i><a href="about.php#About">About</a></li>
-        <li><i class="fas fa-question-circle menuicons"></i><a href="support.php#Support">Support</a></li>
-    </ul>
-</div>
+    <!-- Content -->
+    <div id="content" class="content">
+        <h1>Contact Us</h1>
+        <p class="section-text">If you have any questions, need assistance, or want to provide feedback, please fill out the contact form below. Our support team will respond to your inquiry as soon as possible. We value your input and appreciate your patience.</p>
+        <div class="contact-form-wrapper">
+            <form class="contact-form">
+                <div class="form-group">
+                    <label for="firstName">First Name</label>
+                    <input type="text" class="form-control" id="firstName" name="firstName" required>
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" class="form-control" id="lastName" name="lastName" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="messageType">Reason</label>
+                    <select class="form-control select" id="messageType" name="messageType" required>
+                        <option value="" selected disabled>Select a support reason</option>
+                        <option value="1">Report a Bug</option>
+                        <option value="2">Feedback</option>
+                        <option value="3">Support Inquiry</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+                </div>
+                <button type="submit" class="btn">Submit</button>
+            </form>
+        </div>
+        <!-- Modal dialog box -->
+        <div id="modal" class="modal">
+            <div id="modal-content" class="modal-content">
+                <p>Thank you for contacting us!</p>
+                <button class="modal-close-btn">&times;</button>
+            </div>
+        </div>
+    </div>
 
-<!-- Content -->
-<div id="content" class="content">
-    <h1>Support</h1>
-</div>
-
-<!-- Footer -->
-<footer class="footer">
-    <span>&copy; 2023 DCA</span>
-</footer>
+    <!-- Footer -->
+    <footer class="footer">
+        <span>&copy; 2023 DCA</span>
+    </footer>
 </body>
 </html>
